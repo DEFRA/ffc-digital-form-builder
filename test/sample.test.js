ceedaa672b8c5bcd02712563aa010bcb0e34f0be
John Watson <42574977+johnwatson484@users.noreply.github.com>
2020-08-25 16:32:06 +0100

    Support Jenkins build (#4)

---
+++ test/sample.test.js
@@ -0,0 +1,9 @@
+const Lab = require('lab')
+const { expect } = require('code')
+const lab = exports.lab = Lab.script()
+
+lab.experiment('Sample', () => {
+  lab.test('Sample', () => {
+    expect(true).to.equal(true)
+  })
+})
